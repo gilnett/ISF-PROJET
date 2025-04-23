cb317819ea5618374b2323cd89fc04074b646982
gilnett <142233342+gilnett@users.noreply.github.com>
2025-04-23 20:44:51 +0000

    modifier

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x87ceeb); // Définit la couleur de fond (bleu ciel)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10);
 camera.position.z = 5;
 
